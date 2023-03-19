a4e918679ca294dde7c72c546eb8664bce794177
corgi1118 <126050259+corgi1118@users.noreply.github.com>
2023-03-19 22:40:01 +0800

    Add files via upload

---
+++ hw10/hw10/hw10.cpp
@@ -0,0 +1,37 @@
+﻿// hw10.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include<iostream>
+using namespace std;
+
+int euc(int a, int b);
+int main()
+{
+	int in = 0, in2 = 0, c = 0;
+
+	cin >> in >> in2;
+	if (in < in2)
+	{
+		c = in;
+		in = in2;
+		in2 = c;
+	}
+	cout << euc(in, in2) << endl;
+	return 0;
+}
+int euc(int a, int b)
+{
+	int f = a % b;
+	if (f == 0) return b;
+	else return euc(b, f);
+}
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
